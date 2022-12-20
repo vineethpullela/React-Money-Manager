4ec43dc68766c3be3e8bd9636fa7f63f127d0b1b
vineeth <15072cm031@gmail.com>
2022-12-20 18:18:50 +0000

    money manager app

---
+++ src/components/MoneyManager/index.js
@@ -0,0 +1,73 @@
+import {Component} from 'react'
+import './index.css'
+import MoneyDetails from '../MoneyDetails'
+
+const transactionTypeOptions = [
+  {
+    optionId: 'INCOME',
+    displayText: 'Income',
+  },
+  {
+    optionId: 'EXPENSES',
+    displayText: 'Expenses',
+  },
+]
+
+class MoneyManager extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <div className="money-manager-container">
+          <div className="money-manager-profile-container">
+            <h1 className="profile-name">Hi, Richard</h1>
+            <p className="profile-description">
+              Welcome back to your
+              <span className="money-manager-name"> Money Manager</span>
+            </p>
+          </div>
+          <MoneyDetails />
+          <div className="transaction-details-container">
+            <form className="transaction-from">
+              <h1 className="transaction-heading">Add Transaction</h1>
+              <label htmlFor="title" className="label">
+                TITLE
+              </label>
+              <input
+                type="text"
+                id="title"
+                value=""
+                className="input"
+                placeholder="TITLE"
+              />
+              <label htmlFor="amount" className="label">
+                AMOUNT
+              </label>
+              <input
+                type="text"
+                id="amount"
+                value=""
+                className="input"
+                placeholder="AMOUNT"
+              />
+              <label htmlFor="select" className="label">
+                TYPE
+              </label>
+              <select id="select" className="input">
+                {transactionTypeOptions.map(eachOption => (
+                  <option key={eachOption.optionId} value={eachOption.optionId}>
+                    {eachOption.displayText}
+                  </option>
+                ))}
+              </select>
+              <button type="submit" className="add-button">
+                Add
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MoneyManager
